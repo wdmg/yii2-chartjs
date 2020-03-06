b22645d98a3d31b9df4c892c249a9cecc2b5408b
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-03-06 12:44:45 +0200

    Updating year in copyrights

--- src/ChartJS.php
+++ src/ChartJS.php
@@ -9,7 +9,7 @@ namespace wdmg\widgets;
  * @version         1.0.5
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-chartjs
- * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @copyright       Copyright (c) 2019 - 2020 W.D.M.Group, Ukraine
  * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
  *
  */
